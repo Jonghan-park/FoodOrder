8cd47e5eeb2d5398195bb9c7b58a35f62957fc33
Jonghan Park <jonghan.park@edu.sait.ca>
2022-07-31 23:35:44 -0600

    Create context

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,9 @@ const Header = (props) => {
     <React.Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton>Cart</HeaderCartButton>
+        <HeaderCartButton onClickCartInHeader={props.onClickCart}>
+          Cart
+        </HeaderCartButton>
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImg} alt="A table full of delicious food!" />
